38f6ccbe38f25b8f52ebfb8472ec08405762c4a5
xuxuan198959 <xuxuan198959@gmail.com>
2017-07-11 15:11:09 -0700

    Create Hamming-Distance.java

---
+++ Java/Hamming-Distance.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        int distance = 0;
+        while ((x != 0) || (y != 0)) {
+            if (x%2 != y%2)
+                distance++;
+            x /= 2;
+            y /= 2;
+        }
+        return distance;
+    }
+}
